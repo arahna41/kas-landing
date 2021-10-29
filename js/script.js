eadf610495af0b7d139a8a2e13b1ec4ae7ee8cae
arahna41 <arahna41@gmail.com>
2021-10-29 19:11:10 +0300

    something fixed of style

--- js/script.js
+++ js/script.js
@@ -6,22 +6,3 @@ const myCarousel = new Carousel(document.querySelector(".carousel"), {
   fill: true,
   slidesPerPage : 1,
 });
-
-/* Customize Fancybox */
-Fancybox.bind('[data-fancybox="gallery"]', {
-  Thumbs: false,
-  Toolbar: false,
-  closeButton: "top",
-  center: true,
-  infinite: false,
-  slidesPerPage : 1,
-  Carousel: {
-    on: {
-      change: (that) => {
-        mainCarousel.slideTo(mainCarousel.findPageForSlide(that.page), {
-          friction: 0,
-        });
-      },
-    },
-  },
-});
