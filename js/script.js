d1b2cc195494854c0cf5d37c0e7cb3b2507ad7b5
arahna41 <arahna41@gmail.com>
2021-10-30 20:32:01 +0300

    off carousel in width 768px

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 /* Initialise carousel */
-const myCarousel = new Carousel(document.querySelector(".carousel"), {
-  preload: 2,
-  infinite: false,
-  center: true,
-  fill: true,
-  slidesPerPage : 1,
-});
+  
+
+const mediaQuery = window.matchMedia('(max-width: 767px)')
+if (mediaQuery.matches) {
+  var myCarousel = new Carousel(document.querySelector('.carousel'), {
+    preload: 2,
+    infinite: false,
+    center: true,
+    fill: true,
+    slidesPerPage : 1,
+  });
+}
