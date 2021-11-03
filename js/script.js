827f0596c7a16932e881942cca369280348b0223
arahna41 <arahna41@gmail.com>
2021-11-03 17:45:44 +0200

    add js for nav

--- js/script.js
+++ js/script.js
@@ -10,6 +10,29 @@ if (mediaQuery.matches) {
   });
 }
 
+var linkNav = document.querySelectorAll('[href^="#"]'),
+    V = 0.2; 
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { 
+        e.preventDefault(); 
+        var w = window.pageYOffset,  
+            hash = this.href.replace(/[^#]*(.*)/, '$1'); 
+        t = document.querySelector(hash).getBoundingClientRect().top, 
+            start = null;
+        requestAnimationFrame(step); 
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash 
+            }
+        }
+    }, false);
+}
 
 const btn = document.querySelector(".button");
 const content = document.querySelector(".fixed");
@@ -38,3 +61,5 @@ function fieldClick() {
     console.log(content.classList);
     content.classList.add("fixed_none");
 }
+
+
