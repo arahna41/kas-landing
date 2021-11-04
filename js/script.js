dc4155bf3c448b14df59fc8931fecf1b36e57466
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2021-11-04 14:06:38 +0200

    format js file

--- js/script.js
+++ js/script.js
@@ -11,27 +11,34 @@ if (mediaQuery.matches) {
 }
 
 var linkNav = document.querySelectorAll('[href^="#"]'),
-    V = 0.2; 
+  V = 0.2;
 for (var i = 0; i < linkNav.length; i++) {
-    linkNav[i].addEventListener('click', function(e) { 
-        e.preventDefault(); 
-        var w = window.pageYOffset,  
-            hash = this.href.replace(/[^#]*(.*)/, '$1'); 
-        t = document.querySelector(hash).getBoundingClientRect().top, 
-            start = null;
-        requestAnimationFrame(step); 
-        function step(time) {
-            if (start === null) start = time;
-            var progress = time - start,
-                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-            window.scrollTo(0,r);
-            if (r != w + t) {
-                requestAnimationFrame(step)
-            } else {
-                location.hash = hash 
-            }
+  linkNav[i].addEventListener(
+    "click",
+    function (e) {
+      e.preventDefault();
+      var w = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, "$1");
+      (t = document.querySelector(hash).getBoundingClientRect().top),
+        (start = null);
+      requestAnimationFrame(step);
+      function step(time) {
+        if (start === null) start = time;
+        var progress = time - start,
+          r =
+            t < 0
+              ? Math.max(w - progress / V, w + t)
+              : Math.min(w + progress / V, w + t);
+        window.scrollTo(0, r);
+        if (r != w + t) {
+          requestAnimationFrame(step);
+        } else {
+          location.hash = hash;
         }
-    }, false);
+      }
+    },
+    false
+  );
 }
 
 const btn = document.querySelector(".button");
@@ -40,8 +47,8 @@ const content = document.querySelector(".fixed");
 btn.addEventListener("click", btnClick);
 
 function btnClick() {
-    console.log(content.classList);
-    content.classList.toggle("fixed_none");
+  console.log(content.classList);
+  content.classList.toggle("fixed_none");
 }
 
 const cross = document.querySelector(".close");
@@ -49,8 +56,8 @@ const cross = document.querySelector(".close");
 cross.addEventListener("click", crossClick);
 
 function crossClick() {
-    console.log(content.classList);
-    content.classList.add("fixed_none");
+  console.log(content.classList);
+  content.classList.add("fixed_none");
 }
 
 const field = document.querySelector(".fixed");
@@ -58,8 +65,6 @@ const field = document.querySelector(".fixed");
 field.addEventListener("click", fieldClick);
 
 function fieldClick() {
-    console.log(content.classList);
-    content.classList.add("fixed_none");
+  console.log(content.classList);
+  content.classList.add("fixed_none");
 }
-
-
